c957a0651c7ac7f3f9beb1a1267c79285cd8bf35
IrwanRich <irwanrichwandi001@gmail.com>
2022-12-17 15:55:10 +0700

    initial commit

--- core/src/main/java/com/example/mysubmissionexpert/core/utils/Constanta.kt
+++ core/src/main/java/com/example/mysubmissionexpert/core/utils/Constanta.kt
@@ -1,10 +1,8 @@
 package com.example.mysubmissionexpert.core.utils
 
-import com.example.mysubmissionexpert.core.BuildConfig
-
 class Constanta {
     companion object{
-        const val API_KEY = BuildConfig.API_KEY
+        const val API_KEY = "c342be415b41a870c78c9f566fd8ebbf"
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val TV_BACKDROP = "https://image.tmdb.org/t/p/w500"
         const val TV_POSTER = "https://image.tmdb.org/t/p/original"
